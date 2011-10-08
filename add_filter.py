9bcb6f96257ff6869e6fcfd15aa3e5e9ea33a9fc
Jay Parlar <parlar@gmail.com>
2011-10-07 22:53:01 -0400

    More flexible phone number support
    Now you don't *have* to pass a 10 digit phone number. You can pass any
    kind of formatted 10+ digit number, such as "+1 (905) 555-1234". It will
    extract all the digits, and use the final 10 of them

--- add_filter.py
+++ add_filter.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 from voipms import Voipms
 
+class InvalidCallerid(Exception):
+    pass
+
 def load_accounts(credentials_file):
     accounts = []
     with open(credentials_file) as f:
@@ -9,10 +12,20 @@ def load_accounts(credentials_file):
             accounts.append(Voipms(username, password, ivr))
     return accounts
 
+
+def process_number(callerid):
+    print "Incoming number for filtering", callerid
+    number = filter(lambda x: x.isdigit(), callerid)
+    if len(number) >= 10:
+        return number[-10:]
+    else:
+        raise InvalidCallerid("Received an invalid callerID: %s" % callerid)
+
 def update_filters(accounts, callerid, action, note):
     '''
     Assumes that each Voipms instance in 'accounts' has already been connected
     '''
+    callerid = process_number(callerid)
     for a in accounts:
         a.connect()
         if action == "hangup":
